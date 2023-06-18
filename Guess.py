a9723d3e1d501f3981055477ea495da1df496e5d
River2017CN <river2017cn@gmail.com>
2023-06-18 17:18:40 +0800

    受sb-child启发，开发了谜底揭晓！

--- Guess.py
+++ Guess.py
@@ -3,12 +3,7 @@ chance = 10  #回合次数限制
 guess = ""
 
 def isOK(num1,num2): # 定义函数
-    global chance,guess  #程序怎么run了？把这去掉试试
-    if chance == 0:
-        print("You have no more chances!")
-        print("GAME OVER")
-        return True
-
+    global chance,guess  #程序怎么run了？把这去掉试试！
     if num1 < 0 or num1 > 100:
         print("Out of range. Please try again.")
     elif 0 < num1 < num2:
@@ -27,7 +22,13 @@ num = random.randint(1, 100)  # 在1到100之间取一个随机数
 print("Guess which number am I thinking?(1-100)")
 bingo = False
 
-while not bingo and chance != 0:
+while not bingo:
+    if chance == 0:
+        print("You have no more chances!")
+        print("The answer is " + str(num) + " !")
+        print("GAME OVER")
+        bingo = True
+        break
     guess = input()
     try:
         answer = int(guess)
