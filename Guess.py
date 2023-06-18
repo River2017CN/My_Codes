ff14b26fc8d46e1a3f3cf957a284e9e479f8f4dd
River2017CN <river2017cn@gmail.com>
2023-06-18 15:38:20 +0800

    增加了次数限制功能

--- Guess.py
+++ Guess.py
@@ -1,16 +1,30 @@
-import random  # 导入random模块
+import random  # 导入random模
+chance = 10  #回合次数限制
+guess = 0
 
 def isOK(num1,num2): # 定义函数
-    if num1 < 0 or num1 > 100:
+    global chance,guess
+    if chance == 0:
+        print("You have no more chances!")
+        print("GAME OVER")
+        exit()
+    elif num1 < 0 or num1 > 100:
         print("Out of range. Please try again.")
+        chance = chance - 1
+        print("You have " + str(chance) + " chances left!")
+        return False
     elif 0 < num1 < num2:
         print(guess + " is too small!")
-        return False;
+        chance = chance - 1
+        print("You have " + str(chance) + " chances left!")
+        return False
     elif num1 > num2:
         print(guess + " is too big!")
-        return False;
+        chance = chance - 1
+        print("You have " + str(chance) + " chances left!")
+        return False
     else:
-        print("BINGO! It is " + guess + " ！")
+        print("BINGO! It is " + guess + " !")
         return True
 
 num = random.randint(1, 100)  # 在1到100之间取一个随机数
@@ -18,10 +32,11 @@ num = random.randint(1, 100)  # 在1到100之间取一个随机数
 print("Guess which number am I thinking?(1-100)")
 bingo = False
 
-while not bingo:
-    guess = input()
-    try:
-        answer = int(guess)
+for i in range(chance):  #没必要，但万一，我是说万一，真的有人能突破限制呢？所以我设置了一个兜底措施（迫真）
+    while not bingo and chance != 0:
+        guess = input()
+        try:
+            answer = int(guess)
+        except:
+            print("Wrong input type. Try again.")
         bingo = isOK(answer,num)
-    except:
-        print("Wrong input type. Try again.")
