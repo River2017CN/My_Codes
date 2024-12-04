1568530e2f7e3af3f7d6ae28a385d9d5927d6415
River2017CN <river2017cn@gmail.com>
2024-12-04 22:20:35 +0800

    Maximize improved.

--- Maximize.c
+++ Maximize.c
@@ -1,22 +1,30 @@
-//教材中的求最大值程序
+/*
+教材中的求最大值程序之课后习题，
+由示例的两数取大者变更为了三数求最大者。
+这显然不是最优解，希望我日后可以改进它。
+ToDo：改为可以传入任意个参数的比较程序
+*/
 #include <stdio.h>
+int max(int x,int y,int z)
+{
+    int ans;
+    int cache;
+    if(x>y)cache=x;  //判断
+    else cache=y;
+    if (cache>z)ans=cache;
+    else ans=z;
+    return(ans);
+}
 int main()
 {
-    int max(int x,int y);
-    int a,b,c;
-    scanf("%d,%d",&a,&b);
-    c = max(a,b);
-    printf("The max is %d\n",c);
+    int a,b,c,final;
+    scanf("%d,%d,%d",&a,&b,&c);
+    final = max(a,b,c);
+    printf("The max is %d\n",final);
     return 0;
 }
 
-int max(int x,int y)
-{
-    int z;
-    if(x>y)z=x;  //判断
-    else z=y;
-    return(z);
-}
+
 /*
 运行程序后，输入两个整数变量以比较大小。
 C语言治好了我的空格癌（大嘘
